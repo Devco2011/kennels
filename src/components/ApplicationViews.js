2c6ff2b56088db501c032badbd759ba141572071
Devco2011 <devco2011@gmail.com>
2020-10-05 13:20:07 -0500

    added animal provider, customer provider and refactored

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,12 +1,15 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { Home } from "./Home"
-import { AnimalCard } from "./animal/AnimalCard"
-import { CustomerCard } from "./customers/CustomerCard"
+import { CustomerList } from "./customers/CustomerList"
+import { CustomerProvider } from "./customers/CustomerProvider"
 import { EmployeeCard } from "./employees/EmployeeCard"
 import { LocationCard } from "./locations/LocationCard"
+import { AnimalProvider } from "./animal/AnimalProvider"
+import { AnimalList } from "./animal/AnimalList"
 
-export const ApplicationViews = (props) => {
+
+export const ApplicationViews = () => {
     return (
         <>
             {/* Render the location list when http://localhost:3000/ */}
@@ -15,9 +18,11 @@ export const ApplicationViews = (props) => {
             </Route>
 
             {/* Render the animal list when http://localhost:3000/animals */}
-            <Route path="/animals">
-                <AnimalCard />
-            </Route>
+            <AnimalProvider>
+                <Route exact path="/animals">
+                    <AnimalList />
+                </Route>
+            </AnimalProvider>
 
             {/* Render the location list when http://localhost:3000/locations */}
             <Route path="/locations">
@@ -25,9 +30,11 @@ export const ApplicationViews = (props) => {
             </Route>
 
             {/* Render the customers list when http://localhost:3000/customers */}
-            <Route path="/customers">
-                <CustomerCard />
-            </Route>
+            <CustomerProvider>
+                <Route exact path="/customers">
+                    <CustomerList />
+                </Route>
+            </CustomerProvider>
 
             {/* Render the employee list when http://localhost:3000/employees */}
             <Route path="/employees">
