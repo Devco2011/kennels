887206716564cf90fb3df6900264b14437ec6465
Devco2011 <devco2011@gmail.com>
2020-10-07 10:30:26 -0500

    auth works

--- src/components/locations/LocationProvider.js
+++ src/components/locations/LocationProvider.js
@@ -13,7 +13,7 @@ export const LocationProvider = (props) => {
     const addLocation = locationObj => {
         return fetch("http://localhost:8088/locations", {
             method: "POST",
-            headings: {
+            headers: {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify(locationObj)
