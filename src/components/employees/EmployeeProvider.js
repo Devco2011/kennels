a8607a2dc6239f44b21647ba68f44e3a93fe487c
Devco2011 <devco2011@gmail.com>
2020-10-07 12:51:45 -0500

    can add an employee

--- src/components/employees/EmployeeProvider.js
+++ src/components/employees/EmployeeProvider.js
@@ -11,7 +11,7 @@ export const EmployeeProvider = (props) => {
             .then(setEmployees)
     }
     const addEmployee = employeeObj => {
-        return fetch("http://localhost8088/employees", {
+        return fetch("http://localhost:8088/employees", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
